34349f79541b7a1c5a49bdb640b4e7c87305fa72
desikaaa <desikarmila211@gmail.com>
2025-02-17 16:00:16 +0700

    modif

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
- 
+  // Menampilkan pesan 'Selamat Datang'
+  public function index() {
+    return 'Selamat Datang';
+}
+
+// Menampilkan nama dan NIM
+public function about() {
+    return 'Nama: Desi Karmila, NIM: 123456789';
+}
+
+// Menampilkan halaman artikel dengan id
+public function articles($id) {
+    return "Halaman Artikel dengan Id $id";
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [WelcomeController::class, 'hello']);
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
