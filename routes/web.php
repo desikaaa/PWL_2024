d19b34caf26175e0d9afbd95bc29d5c213a3f6fe
desikaaa <desikarmila211@gmail.com>
2025-02-17 15:01:42 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-
-});
+Route::get('/hello', [WelcomeController::class, 'hello']);
